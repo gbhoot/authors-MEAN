42bf21dd8be288858e9a0ba87441a10fe20df198
Gurpal Bhoot <gsbhoot1020@gmail.com>
2018-12-13 13:33:08 -0800

    server update -prior to postman

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,6 @@
 var path = require('path'),
-    authors = require('../controllers/authors.js');
+    authors = require('../controllers/authors.js'),
+    quotes = require('../controllers/quotes.js');
 
 module.exports = function(app) {
     // Get all the authors
@@ -30,6 +31,41 @@ module.exports = function(app) {
     // Remove one author via id
     app.delete('/db/v1/authors/:id', function(req, res) {
         authors.destroyOne(req, res);
+    });
+
+    // Get all quotes
+    app.get('/db/v1/quotes', function(req, res) {
+        quotes.getAll(req, res);
+    });
+
+    // Get one quote via id
+    app.get('/db/v1/quotes/:id', function(req, res) {
+        quotes.getOne(req, res);
+    });
+
+    // Create a quote for specific author
+    app.post('/db/v1/authors/:id/quotes', function(req, res) {
+        quotes.create(req, res);
+    });
+
+    // Update a quote for specific author (Vote up/down)
+    app.put('/db/v1/authors/:qid/quotes/:aid/:inc', function(req, res) {
+        quotes.update(req, res);
+    });
+
+    // Delete all quotes
+    app.delete('/db/v1/quotes', function(req, res) {
+        quotes.destroyAll(req, res);
+    });
+
+    // Delete one quote via id
+    app.delete('/db/v1/quotes/:id', function(req, res) {
+        quotes.destroyOne(req, res);
+    });
+
+    // Delete one quote for specific author
+    app.delete('/db/v1/authors/:aid/quotes/:qid', function(req, res) {
+        quotes.destroyOneForAuthor(req, res);
     });
 
     // Remaining routes direct to Angular
